f524c64b80433a5b8633542e6cce3813477e315c
Rob Taylor <roboncode@gmail.com>
2023-04-30 22:30:06 -0600

    added prettier

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import { WINDOW, usePortalService } from "@meetportal/portal-js";
+import { WINDOW, usePortalService } from '@meetportal/portal-js'
 
-import { PATIENT_TYPE } from "@meetportal/healthcare-js";
+import { PATIENT_TYPE } from '@meetportal/healthcare-js'
 
-const portal = usePortalService();
+const portal = usePortalService()
 
-portal.subscribe(WINDOW.ON_URL_CHANGE, "*", (url: string) => {
-  if (!url) return;
-  const regex = new RegExp("/\\d+$");
-  const match = url.match(regex);
-  const patientId = (match?.length ? match[0] : "").substring(1);
+portal.subscribe(WINDOW.ON_URL_CHANGE, '*', (url: string) => {
+  if (!url) return
+  const regex = new RegExp('/\\d+$')
+  const match = url.match(regex)
+  const patientId = (match?.length ? match[0] : '').substring(1)
   if (patientId) {
     portal.setResource({
       id: patientId,
       type: PATIENT_TYPE,
-    });
+    })
   } else {
-    portal.clearResource(PATIENT_TYPE);
+    portal.clearResource(PATIENT_TYPE)
   }
-});
+})
