f9f6a41096e7263f4c09c654bdac08e98d11bff7
Rob Taylor <roboncode@gmail.com>
2023-04-30 22:26:43 -0600

    updated portal-js; minified  worker using iife

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,20 @@
-import { WINDOW, usePortalService } from '@meetportal/portal-js'
+import { WINDOW, usePortalService } from "@meetportal/portal-js";
 
-import { PATIENT_TYPE } from '@meetportal/healthcare-js'
+import { PATIENT_TYPE } from "@meetportal/healthcare-js";
 
-async function main() {
-  const portal = usePortalService()
+const portal = usePortalService();
 
-  portal.subscribe(WINDOW.ON_URL_CHANGE, '*', (url: string) => {
-    if (!url) return
-    const regex = new RegExp('/\\d+$')
-    const match = url.match(regex)
-    const patientId = (match?.length ? match[0] : '').substring(1)
-    if (patientId) {
-      portal.setResource({
-        id: patientId,
-        type: PATIENT_TYPE,
-      })
-    } else {
-      portal.clearResource(PATIENT_TYPE)
-    }
-  })
-}
-
-main()
+portal.subscribe(WINDOW.ON_URL_CHANGE, "*", (url: string) => {
+  if (!url) return;
+  const regex = new RegExp("/\\d+$");
+  const match = url.match(regex);
+  const patientId = (match?.length ? match[0] : "").substring(1);
+  if (patientId) {
+    portal.setResource({
+      id: patientId,
+      type: PATIENT_TYPE,
+    });
+  } else {
+    portal.clearResource(PATIENT_TYPE);
+  }
+});
